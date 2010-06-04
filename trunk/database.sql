03cb92cc035252c8378fcd4c27720e0827e99386
barkasn <barkasn@e34f36af-7f7d-4280-9453-604086a0afb0>
2010-06-04 12:15:45 +0000

    Add database structure file

---
+++ trunk/database.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.2.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jun 04, 2010 at 01:06 PM
+-- Server version: 5.1.37
+-- PHP Version: 5.2.10-2ubuntu6.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `arrayline`
+--
+CREATE DATABASE `arrayline` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `arrayline`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataset_processors`
+--
+
+CREATE TABLE IF NOT EXISTS `dataset_processors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `has_no_accept_states` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataset_processors_accept_states`
+--
+
+CREATE TABLE IF NOT EXISTS `dataset_processors_accept_states` (
+  `dataset_processor_id` int(11) NOT NULL,
+  `dataset_state_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataset_processors_produce_states`
+--
+
+CREATE TABLE IF NOT EXISTS `dataset_processors_produce_states` (
+  `dataset_processor_id` int(11) NOT NULL,
+  `dataset_state_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dataset_states`
+--
+
+CREATE TABLE IF NOT EXISTS `dataset_states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `datasets`
+--
+
+CREATE TABLE IF NOT EXISTS `datasets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_id` int(11) DEFAULT NULL,
+  `parent_dataset_id` int(11) DEFAULT NULL,
+  `dataset_state_id` int(11) NOT NULL,
+  `owner_user_id` int(11) NOT NULL,
+  `dataset_processor_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_states`
+--
+
+CREATE TABLE IF NOT EXISTS `job_states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_state_id` int(11) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `autorun` tinyint(1) NOT NULL,
+  `run_start` date NOT NULL,
+  `run_end` date NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE IF NOT EXISTS `modules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules_dataset_processors`
+--
+
+CREATE TABLE IF NOT EXISTS `modules_dataset_processors` (
+  `module_id` int(11) NOT NULL,
+  `dataset_processor_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `process_states`
+--
+
+CREATE TABLE IF NOT EXISTS `process_states` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role_permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `role_permissions` (
+  `roles_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  UNIQUE KEY `roles_id` (`roles_id`,`permission_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `internal_name` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scripts`
+--
+
+CREATE TABLE IF NOT EXISTS `scripts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` varchar(255) NOT NULL,
+  `execution_command` varchar(255) NOT NULL,
+  `can_be_called_directly` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scriptsbodies`
+--
+
+CREATE TABLE IF NOT EXISTS `scriptsbodies` (
+  `script_id` int(11) NOT NULL,
+  `script_body` text NOT NULL,
+  UNIQUE KEY `script_id` (`script_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `user_permissions` (
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`,`permission_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  UNIQUE KEY `user_id` (`user_id`,`role_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `passwordsha1` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `last_access` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
