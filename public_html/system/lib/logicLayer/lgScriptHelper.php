897a64315d3dcdba5920f759f0b3a0d6420e28d7
barkasn <barkasn@e34f36af-7f7d-4280-9453-604086a0afb0>
2010-06-04 13:31:22 +0000

    dbScript updates

--- public_html/system/lib/logicLayer/lgScriptHelper.php
+++ public_html/system/lib/logicLayer/lgScriptHelper.php
@@ -2,15 +2,46 @@
 
 class lgScriptHelper {
 
-	public static function saveScript($name, $body) {
-		//TODO: implement
+	public static function getAllScripts() {
+		global $pdo;
+		$stmt = $pdo->prepare('SELECT id FROM scripts;');
+		$stmt->execute();
+		$dbScripts = array();
+		
+		while ($row = $stmt->fetch()) {
+			$dbScripts[] = new dbScript($row['id']);
+		}
+
+		return $dbScripts;
 	}
 
-	public static function getScript($name) {
+	public static function getScriptByInternalName($internalName) {
+		global $pdo;
+		$stmt = $pdo->prepare('SELECT id FROM scripts WHERE internal_name = :internal_name;');
+		$stmt->bindValue(':internal_name', $internalName);
+		$stmt->execute();
 
-		//TODO: implement
-
+		$dbScript;
+		if ($row = $stmt->fetch()) {
+			$dbScript = new dbScript($row['id']);
+		}
+		return $dbScript;
 	}
+
+	public static function createScript($name) {
+		global $pdo;
+		$stmt = $pdo->prepare('INSERT INTO scripts(internal_name, filename, execution_command, can_be_called_directly) VALUES (:internal_name, :filename, :execution_command, :can_be_called_directly);');
+		$stmt->bindValue(':internal_name', $internalName);
+		$stmt->bindValue(':filename', $filename);
+		$stmt->bindValue(':execution_command', $executionsCommand);
+		$stmt->bindValue(':can_be_called_directly', $canBeCalledDirectly);
+		$stmt->execute();
+
+		$id = $pdo->lastInsertId();
+		return new dbScript($id);
+	}
+
+
 }
 
 	
