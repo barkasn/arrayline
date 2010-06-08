b9dac86dec7c065b0cbbab9e4ab839f26dcda740
barkasn <barkasn@e34f36af-7f7d-4280-9453-604086a0afb0>
2010-06-08 09:36:54 +0000

    Added Basic Database data to sql file

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 07, 2010 at 02:25 PM
+-- Generation Time: Jun 08, 2010 at 10:34 AM
 -- Server version: 5.1.37
 -- PHP Version: 5.2.10-2ubuntu6.4
 
@@ -29,6 +29,14 @@ CREATE TABLE IF NOT EXISTS `dataset_processors` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `dataset_processors`
+--
+
+INSERT INTO `dataset_processors` (`id`, `internal_name`, `name`, `has_no_accept_states`) VALUES
+(1, 'RawDataUpload', 'Raw Data Upload', 1),
+(2, 'FileNameRandomiser', 'File Name Randomiser', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -40,6 +48,13 @@ CREATE TABLE IF NOT EXISTS `dataset_processors_accept_states` (
   `dataset_state_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `dataset_processors_accept_states`
+--
+
+INSERT INTO `dataset_processors_accept_states` (`dataset_processor_id`, `dataset_state_id`) VALUES
+(2, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -50,6 +65,11 @@ CREATE TABLE IF NOT EXISTS `dataset_processors_produce_states` (
   `dataset_processor_id` int(11) NOT NULL,
   `dataset_state_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `dataset_processors_produce_states`
+--
+
 
 -- --------------------------------------------------------
 
@@ -65,6 +85,13 @@ CREATE TABLE IF NOT EXISTS `dataset_states` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `dataset_states`
+--
+
+INSERT INTO `dataset_states` (`id`, `internal_name`, `name`, `description`) VALUES
+(1, 'rawData', 'Raw Data', 'Dataset resulting from the upload of raw data. For Development purposes.');
+
 -- --------------------------------------------------------
 
 --
@@ -79,7 +106,12 @@ CREATE TABLE IF NOT EXISTS `datasets` (
   `owner_user_id` int(11) NOT NULL,
   `dataset_processor_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+
+--
+-- Dumping data for table `datasets`
+--
+
 
 -- --------------------------------------------------------
 
@@ -93,6 +125,21 @@ CREATE TABLE IF NOT EXISTS `job_states` (
   `name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `job_states`
+--
+
+INSERT INTO `job_states` (`id`, `internal_name`, `name`) VALUES
+(1, 'toBeSetup', 'To be setup'),
+(2, 'toBePreprocessed', 'To be preprocessed'),
+(3, 'preprocessing', 'Preprocessing'),
+(4, 'preprocessedHalted', 'Preprocessed Halted'),
+(5, 'toBeRun', 'To be Run'),
+(6, 'processRunning', 'Process Running'),
+(7, 'processComplete', 'Process Complete'),
+(8, 'postProcessing', 'Post-processing'),
+(9, 'complete', 'Job Complete');
 
 -- --------------------------------------------------------
 
@@ -109,7 +156,12 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `run_end` date NOT NULL,
   `comment` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=51 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=89 ;
+
+--
+-- Dumping data for table `jobs`
+--
+
 
 -- --------------------------------------------------------
 
@@ -124,6 +176,14 @@ CREATE TABLE IF NOT EXISTS `modules` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `internal_name`, `name`) VALUES
+(1, 'RawDataUpload', 'Raw Data Upload Module'),
+(2, 'FileNameRandomiser', 'Randomises File Names');
+
 -- --------------------------------------------------------
 
 --
@@ -134,6 +194,14 @@ CREATE TABLE IF NOT EXISTS `modules_dataset_processors` (
   `module_id` int(11) NOT NULL,
   `dataset_processor_id` int(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `modules_dataset_processors`
+--
+
+INSERT INTO `modules_dataset_processors` (`module_id`, `dataset_processor_id`) VALUES
+(1, 1),
+(2, 2);
 
 -- --------------------------------------------------------
 
@@ -149,6 +217,13 @@ CREATE TABLE IF NOT EXISTS `permissions` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `internal_name`, `name`, `description`) VALUES
+(1, 'manageusers', 'Manage User Accounts', 'Manage and Administer User Acoounts');
+
 -- --------------------------------------------------------
 
 --
@@ -163,6 +238,11 @@ CREATE TABLE IF NOT EXISTS `process_states` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+--
+-- Dumping data for table `process_states`
+--
+
+
 -- --------------------------------------------------------
 
 --
@@ -174,6 +254,11 @@ CREATE TABLE IF NOT EXISTS `role_permissions` (
   `permission_id` int(11) NOT NULL,
   UNIQUE KEY `roles_id` (`roles_id`,`permission_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `role_permissions`
+--
+
 
 -- --------------------------------------------------------
 
@@ -188,6 +273,11 @@ CREATE TABLE IF NOT EXISTS `roles` (
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `roles`
+--
+
 
 -- --------------------------------------------------------
 
@@ -204,6 +294,14 @@ CREATE TABLE IF NOT EXISTS `scripts` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Dumping data for table `scripts`
+--
+
+INSERT INTO `scripts` (`id`, `internal_name`, `filename`, `execution_command`, `can_be_called_directly`) VALUES
+(1, 'randomizer', 'randomizer.sh', './ randomizer.sh', 1),
+(2, 'randomizerhelper', 'randomizerhelper.sh', '', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -215,6 +313,14 @@ CREATE TABLE IF NOT EXISTS `scripts_bodies` (
   `script_body` text NOT NULL,
   UNIQUE KEY `script_id` (`script_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `scripts_bodies`
+--
+
+INSERT INTO `scripts_bodies` (`script_id`, `script_body`) VALUES
+(1, 'Randomizer body'),
+(2, 'Randomizer Helper body');
 
 -- --------------------------------------------------------
 
@@ -228,6 +334,13 @@ CREATE TABLE IF NOT EXISTS `user_permissions` (
   UNIQUE KEY `user_id` (`user_id`,`permission_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `user_permissions`
+--
+
+INSERT INTO `user_permissions` (`user_id`, `permission_id`) VALUES
+(1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -239,6 +352,11 @@ CREATE TABLE IF NOT EXISTS `user_roles` (
   `role_id` int(11) NOT NULL,
   UNIQUE KEY `user_id` (`user_id`,`role_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_roles`
+--
+
 
 -- --------------------------------------------------------
 
@@ -254,4 +372,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `last_access` datetime NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `passwordsha1`, `created`, `last_access`) VALUES
+(1, 'nikolas', 'a28cc654d85c1d3cb8418061db20859c322a0bc6', '2010-05-26 00:00:00', '2010-05-26 00:00:00');
 
