0a4227984b51b17bad65340a26bfed2ce5828651
barkasn <barkasn@e34f36af-7f7d-4280-9453-604086a0afb0>
2010-06-17 10:28:05 +0000

    Added missing file

---
+++ trunk/public_html/system/lib/logicLayer/requestHandlers/lgReqHandlerAbout.php
@@ -0,0 +1,41 @@
+<?php
+// $Id: lgReqHandlerDefault.php 95 2010-06-16 15:42:47Z barkasn $
+
+/*
+
+    Arrayline, an extensible bioinfomatics data processing platform
+    Copyright (C) 2010 Nikolaos Barkas
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+ 
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+*/
+
+class lgReqHandlerAbout implements iRequestHandler {
+	public function __construct() {
+
+	}
+
+	public function processRequest(lgRequest $lgRequest) {
+		$page = new lgCmsPage();
+		$page->setTitle('About Page');
+		$page->appendContent('About text');
+		$page->render();
+	}
+
+	public function getRequiredPermissions() {
+		return array();
+	}
+}
+
