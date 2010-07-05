9890d08d2ba906488b7fac03a70bcdc4e2945b48
barkasn <barkasn@e34f36af-7f7d-4280-9453-604086a0afb0>
2010-07-05 15:33:55 +0000

    Added normalisation options

--- trunk/public_html/modules/mdlAffymetrixNormalisation/dspAffymetrixNormalisation.php
+++ trunk/public_html/modules/mdlAffymetrixNormalisation/dspAffymetrixNormalisation.php
@@ -79,6 +79,11 @@ select continue. This will schedule a new job for background running</p>');
 				<option value="gcrma">GCRMA</option>
 				<option value="rma">RMA</option>
 				<option value="vsnrma">VSNRMA</option>
+				<option value="quantiles">Quantiles</option>
+				<option value="invariantset">Non-linear</option>
+				<option value="cyclicloess">Cyclic loess</option>
+				<option value="contrast">Contrast</option>
+				<option value="mas5">MAS5</option>
 			</select>');
 		$form->addField($algorithmDropDown);
 		//TODO: Add Hidden Fields
@@ -131,22 +136,37 @@ select continue. This will schedule a new job for background running</p>');
 	}
 
 	private function getNormalisationScriptByAlgorithm($algorithmName) {
+		$scriptName = '';
+	
 		switch($algorithmName) {
 			case 'gcrma':
-				return 'norm_gcrma';
+				$scriptName = 'norm_gcrma';
 				break;
 			case 'rma':
-				return 'norm_rma';
+				$scriptName = 'norm_rma';
 				break;
 			case 'vsnrma':
-				return 'norm_rma';
+				$scriptName = 'norm_rma';
 				break;
 			case 'mas5':
-				return 'norm_mas5';
+				$scriptName = 'norm_mas5';
 				break;
+			case 'quantiles':
+				$scriptName = 'norm_quantiles';
+				break;
+			case 'invariantset':
+				$scriptName = 'norm_invariantset';
+				break;
+			case 'cyclicloess':
+				$scriptName = 'norm_cyclicloess';
+				break;
+			case 'contrast':
+				$scriptName = 'norm_contrast';
 			default:
 				throw new Exception('Invalid Normalisation parameter');
 		}
+
+		return $scriptName;
 	}
 
 
